7c5adb6ed4d60519adbe4b3c43951235aa385171
Matt Raible <matt.raible@okta.com>
2019-12-05 18:50:29 -0700

    Add CORS Filter

--- notes-api/src/main/kotlin/com/okta/developer/notes/DemoApplication.kt
+++ notes-api/src/main/kotlin/com/okta/developer/notes/DemoApplication.kt
@@ -3,18 +3,39 @@ package com.okta.developer.notes
 import com.fasterxml.jackson.annotation.JsonIgnore
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.FilterRegistrationBean
+import org.springframework.context.annotation.Bean
+import org.springframework.core.Ordered
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.rest.core.annotation.HandleBeforeCreate
 import org.springframework.data.rest.core.annotation.RepositoryEventHandler
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Component
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.Id
 
 @SpringBootApplication
-class DemoApplication
+class DemoApplication {
+
+    @Bean
+    fun simpleCorsFilter(): FilterRegistrationBean<CorsFilter> {
+        val source = UrlBasedCorsConfigurationSource()
+        val config = CorsConfiguration()
+        config.allowCredentials = true
+        config.allowedOrigins = listOf("http://localhost:4200")
+        config.allowedMethods = listOf("*");
+        config.allowedHeaders = listOf("*")
+        source.registerCorsConfiguration("/**", config)
+        val bean = FilterRegistrationBean(CorsFilter(source))
+        bean.order = Ordered.HIGHEST_PRECEDENCE
+        return bean
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<DemoApplication>(*args)
