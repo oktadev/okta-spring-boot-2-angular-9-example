f948f08c2b223e445e89cff543746c727bef3a75
Matt Raible <matt.raible@okta.com>
2019-12-07 09:39:23 -0700

    DRY service

--- notes/src/app/note/note.service.ts
+++ notes/src/app/note/note.service.ts
@@ -4,17 +4,19 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 
+const headers = new HttpHeaders().set('Accept', 'application/json');
+
 @Injectable()
 export class NoteService {
   noteList: Note[] = [];
+  api = 'http://localhost:8080/api/notes';
 
   constructor(private http: HttpClient) {
   }
 
   findById(id: string): Observable<Note> {
-    const url = `http://localhost:8080/api/notes/${id}`;
-    const params = { 'id': id };
-    const headers = new HttpHeaders().set('Accept', 'application/json');
+    const url = `${this.api}/${id}`;
+    const params = { id };
     return this.http.get<Note>(url, {params, headers});
   }
 
@@ -29,26 +31,22 @@ export class NoteService {
   }
 
   find(filter: NoteFilter): Observable<Note[]> {
-    const url = `http://localhost:8080/user/notes`;
-    const headers = new HttpHeaders().set('Accept', 'application/json');
-
     const params = {
       title: filter.title,
     };
 
-    return this.http.get<Note[]>(url, {params, headers});
+    return this.http.get<Note[]>(this.api, {params, headers});
   }
 
   save(entity: Note): Observable<Note> {
     let params = new HttpParams();
     let url = '';
-    const headers = new HttpHeaders().set('content-type', 'application/json');
     if (entity.id) {
-      url = `http://localhost:8080/api/notes/${entity.id.toString()}`;
+      url = `${this.api}/${entity.id.toString()}`;
       params = new HttpParams().set('ID', entity.id.toString());
       return this.http.put<Note>(url, entity, {headers, params});
     } else {
-      url = `http://localhost:8080/api/notes`;
+      url = `${this.api}`;
       return this.http.post<Note>(url, entity, {headers, params});
     }
   }
@@ -56,9 +54,8 @@ export class NoteService {
   delete(entity: Note): Observable<Note> {
     let params = new HttpParams();
     let url = '';
-    const headers = new HttpHeaders().set('content-type', 'application/json');
     if (entity.id) {
-      url = `http://localhost:8080/api/notes/${entity.id.toString()}`;
+      url = `${this.api}/${entity.id.toString()}`;
       params = new HttpParams().set('ID', entity.id.toString());
       return this.http.delete<Note>(url, {headers, params});
     }
