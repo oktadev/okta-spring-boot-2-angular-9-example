1e962872549da7c5f6d82371287c2884a745bf2c
Matt Raible <matt.raible@okta.com>
2020-01-05 16:24:31 -0600

    Fix indentation

--- notes-api/src/main/kotlin/com/okta/developer/notes/DemoApplication.kt
+++ notes-api/src/main/kotlin/com/okta/developer/notes/DemoApplication.kt
@@ -22,45 +22,45 @@ import javax.persistence.Id
 @SpringBootApplication
 class DemoApplication {
 
-	@Bean
-	fun simpleCorsFilter(): FilterRegistrationBean<CorsFilter> {
-		val source = UrlBasedCorsConfigurationSource()
-		val config = CorsConfiguration()
-		config.allowCredentials = true
-		config.allowedOrigins = listOf("http://localhost:4200")
-		config.allowedMethods = listOf("*");
-		config.allowedHeaders = listOf("*")
-		source.registerCorsConfiguration("/**", config)
-		val bean = FilterRegistrationBean(CorsFilter(source))
-		bean.order = Ordered.HIGHEST_PRECEDENCE
-		return bean
-	}
+    @Bean
+    fun simpleCorsFilter(): FilterRegistrationBean<CorsFilter> {
+        val source = UrlBasedCorsConfigurationSource()
+        val config = CorsConfiguration()
+        config.allowCredentials = true
+        config.allowedOrigins = listOf("http://localhost:4200")
+        config.allowedMethods = listOf("*");
+        config.allowedHeaders = listOf("*")
+        source.registerCorsConfiguration("/**", config)
+        val bean = FilterRegistrationBean(CorsFilter(source))
+        bean.order = Ordered.HIGHEST_PRECEDENCE
+        return bean
+    }
 }
 
 
 fun main(args: Array<String>) {
-	runApplication<DemoApplication>(*args)
+    runApplication<DemoApplication>(*args)
 }
 
 @Entity
 data class Note(@Id @GeneratedValue var id: Long? = null,
-				var title: String? = null,
-				var text: String? = null,
-				@JsonIgnore var user: String? = null)
+                var title: String? = null,
+                var text: String? = null,
+                @JsonIgnore var user: String? = null)
 
 @RepositoryRestResource
 interface NotesRepository : JpaRepository<Note, Long> {
-	fun findAllByUser(name: String): List<Note>
+    fun findAllByUser(name: String): List<Note>
 }
 
 @Component
 @RepositoryEventHandler(Note::class)
 class AddUserToNote {
 
-	@HandleBeforeCreate
-	fun handleCreate(note: Note) {
-		val username: String =  SecurityContextHolder.getContext().getAuthentication().name
-		println("Creating note: $note with user: $username")
-		note.user = username
-	}
+    @HandleBeforeCreate
+    fun handleCreate(note: Note) {
+        val username: String = SecurityContextHolder.getContext().getAuthentication().name
+        println("Creating note: $note with user: $username")
+        note.user = username
+    }
 }
